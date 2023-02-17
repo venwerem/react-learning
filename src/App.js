ad723deb7de579279a502ffc4dfcaae1613dc745
DanielAmah <amahdanieljack@gmail.com>
2023-02-17 18:38:01 -0500

    A simple add and substraction app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+// This is called a fragment in React
+{/* <></> */}
+
+// React Life cycle
+
+// When an action is carried out,
+// The state of the application changes
+// The app re-renders to update the application
+
+// React Hooks
+
+// useState
+// 1. Initial State
+// 2. Set the new state
+
+// useEffect
+
+
+const App = () => {
+  const [value, setValue] = useState(5)
+
+  const handleSubstraction = () => {
+    setValue(value - 1)
+  }
+
+  const handleAddition = () => {
+    setValue(value + 1)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    <p>{value}</p>
+    <button onClick={handleSubstraction}>-</button>
+    <button onClick={handleAddition}>+</button>
+  </div>)
 }
+
+
+
 
 export default App;
