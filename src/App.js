ed0680452e1430e3714900a8187bfa5a12412abd
DanielAmah <amahdanieljack@gmail.com>
2023-02-22 19:28:13 -0500

    simple weather

--- src/App.js
+++ src/App.js
@@ -1,46 +1,64 @@
-import logo from './logo.svg';
-import React, {useState} from 'react';
-import './App.css';
 
-// This is called a fragment in React
-{/* <></> */}
+import React, {useState, useEffect} from 'react';
 
-// React Life cycle
 
-// When an action is carried out,
-// The state of the application changes
-// The app re-renders to update the application
-
-// React Hooks
-
-// useState
-// 1. Initial State
-// 2. Set the new state
-
-// useEffect
-
+// Whatever is in the useEffect logic runs when
+// When the app loads for the first time.
+// Within my useEffect, do something that causes the state to change
+// And re-render the application
+// When using useEffect, you need to add a dependency
+// A dependency is value or set of value that trigger a re-renders once it is changed
 
 const App = () => {
-  const [value, setValue] = useState(5)
+ const [location, setLocation] = useState("")
+ const [longitude, setLongitude] = useState("")
+ const [latitude, setLatitude] = useState("")
+ const [weather, setWeather] = useState("")
 
-  const handleSubstraction = () => {
-    setValue(value - 1)
+  useEffect(() => {
+    loadInitialWeather()
+  }, [])
+
+
+  const loadInitialWeather = async (location = 'North York') => {
+    const response = await fetch(`https://nominatim.openstreetmap.org/search.php?q=${location}&format=jsonv2`)
+    const data = await response.json();
+    const firstData = data[0]
+    setLongitude(firstData.lon)
+    setLatitude(firstData.lat)
+    const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${firstData.lat}&longitude=${firstData.lon}&current_weather=true`)
+    const weatherData = await weatherResponse.json();
+
+    console.log(weatherData, 'weatherData');
+    setWeather(weatherData.current_weather.temperature)
   }
 
-  const handleAddition = () => {
-    setValue(value + 1)
+  const handleLocation = async (event) => {
+    console.log(event, 'event');
+    setLocation(event.target.value)
+    loadInitialWeather(event.target.value)
+   
   }
-
 
   return (
-  <div>
-    <p>{value}</p>
-    <button onClick={handleSubstraction}>-</button>
-    <button onClick={handleAddition}>+</button>
-  </div>)
+    <>
+    <input value={location} onChange={handleLocation} />
+
+    <p>{location || 'North York'}</p>
+    <p>{latitude}</p>
+    <p>{longitude}</p>
+    <p>Current weather is: {weather}</p>
+    </>
+  )
 }
 
 
 
 
 export default App;
+
+
+// API Weather
+// https://api.open-meteo.com/v1/forecast?latitude=43.7543263&longitude=-79.44911696639593&current_weather=true
+// Get Latitude/Longitude
+// https://nominatim.openstreetmap.org/search.php?q=North York&format=jsonv2
---
+++ src/Calculator.js
@@ -0,0 +1,96 @@
+
+import React, {useState} from 'react';
+import './App.css';
+
+// This is called a fragment in React
+//{/* <></> */}
+
+// React Life cycle
+
+// When an action is carried out,
+// The state of the application changes
+// The app re-renders to update the application
+
+// React Hooks
+
+// useState
+// 1. Initial State
+// 2. Set the new state
+
+// useEffect
+
+
+const Calculator = () => {
+  //const [answer, setAnswer] = useState(0)
+  const [value, setvalue] = useState(0)
+  //const [secondValue, setsecondValue] = useState(0)
+  const [calculation, setCalculation] = useState('')
+
+  const handleButton= (secondvalue)=>
+  {
+    setvalue(value + secondvalue);
+  }
+
+
+  const handleAddition = () => {
+   setCalculation('+')
+  }
+
+  const handleSubtraction = () =>{
+    setCalculation('-')
+  }
+
+  const handleResult = () => {
+   if(calculation === '+') {
+    //const result = value + secondValue;
+  
+    //setAnswer(result)
+   }
+   
+   if(calculation === '-'){
+    //const result = value - secondValue;
+    //setAnswer(result)
+   }
+  }
+
+  
+
+  
+
+
+  return (
+  <div>
+    <p>{'I am a calculator'}</p>
+  
+    <button onClick={handleButton(1)}>1</button>
+    <button onClick={handleButton(2)}>2</button>
+    <button onClick={handleButton(3)}>3</button>
+    <br/>
+    <button onClick={handleButton(4)}>4</button>
+    <button onClick={handleButton(5)}>5</button>
+    <button onClick={handleButton(6)}>6</button>
+    <br/>
+    <button onClick={handleButton(7)}>7</button>
+    <button onClick={handleButton(8)}>8</button>
+    <button onClick={handleButton(9)}>9</button>
+    <button onClick={handleButton(0)}>0</button>
+    
+    <button onClick={handleSubtraction}>-</button>
+   
+    <button onClick={handleAddition}>+</button>
+    <button onClick={handleResult}>=</button>
+    <br/>
+    
+  </div>)
+}
+
+
+
+
+export default Calculator;
+
+
+// API Weather
+// https://api.open-meteo.com/v1/forecast?latitude=40.714&longitude=-74.006&current_weather=true
+// Get Latitude/Longitude
+// https://nominatim.openstreetmap.org/search.php?q=North York&format=jsonv2
