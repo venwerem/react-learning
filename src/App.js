ff438102e2afe2a3993a79272b680a0ac42a900a
Vivian <enweremvivian26@yahoo.com>
2023-03-01 12:50:59 -0500

    Calculator update

--- src/App.js
+++ src/App.js
@@ -20,71 +20,74 @@ import './App.css';
 // useEffect
 
 
-const App = () => {
-  //const [answer, setAnswer] = useState(0)
-  const [value, setvalue] = useState(0)
-  //const [secondValue, setsecondValue] = useState(0)
-  const [calculation, setCalculation] = useState('')
+const App =() => {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
 
-  const handleButton= (secondvalue)=>
-  {
-    setvalue(value + secondvalue);
+  const handleClick = (e) => {
+    setInput(input + e.target.name);
   }
 
-
-  const handleAddition = () => {
-   setCalculation('+')
+  const handleClear = () => {
+    setInput("");
+    setResult("");
   }
 
-  const handleSubtraction = () =>{
-    setCalculation('-')
+  const handleBackspace = () => {
+    setInput(input.slice(0, -1));
   }
 
-  const handleResult = () => {
-   if(calculation === '+') {
-    //const result = value + secondValue;
-  
-    //setAnswer(result)
-   }
-   
-   if(calculation === '-'){
-    //const result = value - secondValue;
-    //setAnswer(result)
-   }
+  const handleEqual = () => {
+    try {
+      // eslint-disable-next-line no-eval
+      setResult(eval(input).toString());
+    } catch (error) {
+      setResult("Error");
+    }
   }
-
-  
-
-  
-
 
   return (
-  <div>
-    <p>{}</p>
-  
-    <button onClick={handleButton(1)}>1</button>
-    <button onClick={handleButton(2)}>2</button>
-    <button onClick={handleButton(3)}>3</button>
-    <br/>
-    <button onClick={handleButton(4)}>4</button>
-    <button onClick={handleButton(5)}>5</button>
-    <button onClick={handleButton(6)}>6</button>
-    <br/>
-    <button onClick={handleButton(7)}>7</button>
-    <button onClick={handleButton(8)}>8</button>
-    <button onClick={handleButton(9)}>9</button>
-    <button onClick={handleButton(0)}>0</button>
-    
-    <button onClick={handleSubtraction}>-</button>
-   
-    <button onClick={handleAddition}>+</button>
-    <button onClick={handleResult}>=</button>
-    <br/>
-    
-  </div>)
+    <div className="App">
+      <div className="calculator">
+        <div className="display">
+          <input type="text" value={input} />
+          <br/>
+          <span>{result}</span>
+        </div>
+        <div className="keypad">
+          <button name="1" onClick={handleClick}>1</button>
+          <button name="2" onClick={handleClick}>2</button>
+          <button name="3" onClick={handleClick}>3</button>
+          <br/>
+          <button name="4" onClick={handleClick}>4</button>
+          <button name="5" onClick={handleClick}>5</button>
+          <button name="6" onClick={handleClick}>6</button>
+          <br/>
+          <button name="7" onClick={handleClick}>7</button>
+          <button name="8" onClick={handleClick}>8</button>
+          <button name="9" onClick={handleClick}>9</button>
+          <br/>
+          <button name="0" onClick={handleClick}>0</button>
+          <button name="-" onClick={handleClick}>&ndash;</button>
+          <button name="+" onClick={handleClick}>+</button>
+          <br/>
+          <button name="/" onClick={handleClick}>&divide;</button>
+          <button name="*" onClick={handleClick}>&times;</button>
+          <button className="equal" onClick={handleEqual}>=</button>
+          <br/>
+          <button name="." onClick={handleClick}>.</button>
+          <button className="clear" onClick={handleClear}>C</button>
+          <button className="backspace" onClick={handleBackspace}>CE</button>
+          
+         
+        </div>
+      </div>
+    </div>
+  );
 }
 
-
-
-
 export default App;
+
+
+
+
